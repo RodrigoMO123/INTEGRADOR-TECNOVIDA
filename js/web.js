ac983ea71fc31041befa633066dba55771659b15
RodrigoMO123 <93835265+RodrigoMO123@users.noreply.github.com>
2025-07-10 23:26:38 -0500

    Add files via upload

---
+++ js/web.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+ 
+    const apkUrl = "https://download1076.mediafire.com/n7i4kqfz35lg8gzfoG8FalTbL4rizSTKYGn6xZ9d1chPXmEr0oxAE-0GatdvBE9OJdn4oGv-30Ml1ZNf5DZuLUWn5aeJrXRMzIWGN5ixfe7xByLa66crKW41jRuUudyjBWG-OZogeUSJVDkXpsHssomW942aq9SLrb22TxYI3gRRkEpD/in36xnid7fqsugw/app-release.apk";
+
+    const downloadButton = document.querySelector('.download-button');
+    const qrCodeImage = document.getElementById('qrCodeImage');
+
+});
